71214a2b70b6f4f5c43661dfc34f6f0a20677a92
Swagatika Sahoo <swagatikasun12@gmail.com>
2021-02-01 15:23:27 +0530

    Resolved typos in middleware fabric sdk

--- middleware/fabric/land_cc/index.js
+++ middleware/fabric/land_cc/index.js
@@ -1,6 +1,6 @@
 const CreateLand = require("./createLand");
 const GetLand = require("./getLand");
-const TransferLand = require("./updateLand");
+const TransferLand = require("./transferLand");
 const GetLands = require("./getLands");
 const payload = {
     CreateLand,
